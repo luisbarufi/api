955a12e8be734155394994531dda5415001519a3
Luis Barufi <luisbarufi@gmail.com>
2024-03-07 21:51:50 -0300

    Add the Authentication module part one

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: `U2|;cfU-^6uG@nYUwslut,xtdp3DmP$G`,
     }),
+    UserModule,
+    PrismaService,
   ],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   async createToken() {
     // return this.jwtService.sign();
@@ -12,4 +16,48 @@ export class AuthService {
   async checkToken() {
     // return this.jwtService.verify();
   }
+
+  async login(email: string, password: string) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email,
+        password,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid password or email.');
+    }
+
+    return user;
+  }
+
+  async forget(email: string) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Email is incorrect.');
+    }
+
+    // TODO: Send the email.
+
+    return true;
+  }
+
+  // async reset(password: string, token: string) {
+  //   // TODO: Validate the token.
+
+  //   const id = 0;
+
+  //   await this.prisma.user.update({
+  //     where: { id },
+  //     data: { password },
+  //   });
+
+  //   return true;
+  // }
 }
